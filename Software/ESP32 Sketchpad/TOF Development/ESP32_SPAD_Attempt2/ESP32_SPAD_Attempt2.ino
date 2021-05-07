b19545d1cb29ec124c1e5d8f422dbb7cee00f00f
saadkazi <saad.i.kazi@gmail.com>
2021-05-07 14:50:22 -0400

    Able to initialized Dev Kit VL53L1X using Pololu <VL53L1X.h>sensor.init() function
    See Software\ESP32 Sketchpad\TOF Development\ESP32_SPAD_Attempt2
    
    Just initialized the sensor successfully without using "sensor.setTimeout(500);" <-- I'm worried this might cause the sensor to freeze.
    
    Next step is to use
    
      sensor.setDistanceMode(VL53L1X::Long);
      sensor.setMeasurementTimingBudget(50000);

--- Software/ESP32 Sketchpad/TOF Development/ESP32_SPAD_Attempt2/ESP32_SPAD_Attempt2.ino
+++ Software/ESP32 Sketchpad/TOF Development/ESP32_SPAD_Attempt2/ESP32_SPAD_Attempt2.ino
@@ -1,5 +1,12 @@
 #include <Wire.h>
  
+#include <VL53L1X.h>  // from pololu library
+
+VL53L1X sensor;
+
+int TOF_discovered_flag = 0; // set to 1 when the TOF address 0x29 is discovered 
+int initialize_once_flag = 0; // used so the TOF sensor is initialized once
+
  
 void setup()
 {
@@ -17,39 +24,67 @@ void loop()
   byte error, address;
   int nDevices;
  
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
+
+
+  if(TOF_discovered_flag == 0)  // find the address for the TOF
   {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
+      Serial.println("Scanning...");
+    
+      nDevices = 0;
+      for(address = 1; address < 127; address++ )
+      {
+        // The i2c_scanner uses the return value of
+        // the Write.endTransmisstion to see if
+        // a device did acknowledge to the address.
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+     
+        if (error == 0)
+        {
+          Serial.print("I2C device found at address 0x");
+          if (address<16)
+            Serial.print("0");
+          Serial.print(address,HEX);
+          Serial.println("  !");
+    
+          if(address == 41)
+          {
+            Serial.println("VL53L1X discovered  !");
+            TOF_discovered_flag = 1;
+          }
+     
+          nDevices++;
+        }
+        else if (error==4)
+        {
+          Serial.print("Unknown error at address 0x");
+          if (address<16)
+            Serial.print("0");
+          Serial.println(address,HEX);
+        }    
+      }
+      if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+      else
+        Serial.println("done\n");
+     
+      delay(5000);           // wait 5 seconds for next scan
   }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
- 
-  delay(5000);           // wait 5 seconds for next scan
+
+  // after finding the address, initialize the sensor
+  if(initialize_once_flag == 0)
+  {
+    if (!sensor.init())
+    {
+      Serial.println("Failed to detect and initialize sensor!");
+
+    }
+    else
+    {
+      Serial.println("VL53L1X is initialized");
+    }
+    initialize_once_flag = 1;
+    
+  }
+
 }
